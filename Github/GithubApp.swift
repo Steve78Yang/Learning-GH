72b1eea4692cacc84c297d62ea6928c81693ba63
양석모 <nepastel78@gmail.com>
2021-09-03 09:23:01 +0900

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by 양석모 on 2021/09/03.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
